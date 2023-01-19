9c4b771b038495ef0c0a2e733d337c398d3a4bdc
Nara Lice <115565847+NaraLice64@users.noreply.github.com>
2023-01-18 22:15:25 -0300

    Update LoginFalha.java

--- LoginFalha.java
+++ LoginFalha.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-@DisplayName("Teste Web com falha")
+@DisplayName("Teste Web Falha")
 public class LoginFalha {
     @Test
-    @DisplayName("Fazer login com falha")
-    public void testLoginDeFalha() {
+    @DisplayName("FazerLogin")
+    public void testValidarLoginDeFalha() {
         // Abrir o navegador
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver_win32\\chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
@@ -21,15 +21,19 @@ public class LoginFalha {
         //Navegar para a pagina da Product Store
         navegador.get("https://www.demoblaze.com/index.html");
 
-        // Fazer login
+        // Clicar no botão Log in
         navegador.findElement(By.cssSelector("a[id='login2']")).click();
+
+        // Inserir Username
         navegador.findElement(By.id("loginusername")).sendKeys("uxgroup");
 
-        navegador.findElement(By.cssSelector("input[id='loginpassword']")).click();
-        navegador.findElement(By.id("loginpassword")).sendKeys("12345");
+        // Inserir Password
+        navegador.findElement(By.id("loginpassword")).sendKeys("123456");
 
-        navegador.findElement(By.cssSelector(" button[onclick='logIn()']")).click();
+        // Clicar no botão Log in
+        navegador.findElement(By.cssSelector("button[onclick='logIn()']")).click();
 
 
     }
 }
+
